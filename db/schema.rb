452c216bbe2de00d2bb97c912fd5fa5569b93952
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2021-05-05 09:43:47 -0600

    edited files to include column for condemn and other items for category column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210410141454) do
+ActiveRecord::Schema.define(version: 20210505153606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20210410141454) do
     t.string   "serial"
     t.decimal  "price",              precision: 8, scale: 2
     t.string   "consumable"
+    t.string   "condemn"
   end
 
   create_table "members", force: :cascade do |t|
